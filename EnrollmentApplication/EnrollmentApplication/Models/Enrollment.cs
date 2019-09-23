3e958f05898324430afb51b79c019720e0dd4481
Jay Singh <s01161093@acad.tri-c.edu>
2019-09-22 23:52:20 -0400

    ef0415ff03a0382484a410502cf87aba5db059d3

---
+++ EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnrollmentApplication.Models
+{
+    public class Enrollment
+    {
+        public virtual int EnrollmentId { get; set; }
+        public virtual int StudentId { get; set; }
+        public virtual int CourseId { get; set; }
+        public virtual double Grade { get; set; }
+        public virtual string StudentObject { get; set; }
+        public virtual string CourseObject { get; set; }
+    }
+}
---
+++ EnrollmentApplication/EnrollmentApplication/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnrollmentApplication.Models
+{
+    public class Student
+    {
+
+        public virtual int StudentId { get; set; }
+        public virtual string firstName { get; set; }
+        public virtual string lastName { get; set; }
+
+    }
+}
