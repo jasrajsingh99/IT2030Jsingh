6721a297473a762cb25db3741cd661c94b605469
Jay Singh <s01161093@acad.tri-c.edu>
2019-10-06 21:03:14 -0400

    1aee2a92

--- EnrollmentApplication/EnrollmentApplication/Models/Course.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Course.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace EnrollmentApplication.Models
 {
     public class Course
     {
+        [Display(Name = "Course ID")]
         public virtual int CourseId { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
+        [Display(Name = "Course Title")]
+        [StringLength(150, ErrorMessage = "Please enter a short title")]
         public virtual string Title { get; set; }
+
+        [Display(Name = "Description")]
         public virtual string Description { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
+        [Range(typeof(int), "1", "4", ErrorMessage = "Please enter credits in a range of 1 to 4")]
+        [Display(Name = "Number of Credits")]
         public virtual int Credits { get; set; }
 
     }
--- EnrollmentApplication/EnrollmentApplication/Models/Student.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Student.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace EnrollmentApplication.Models
 {
     public class Student
     {
-
+        [Display(Name = "Student ID")]
         public virtual int StudentId { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
+        [StringLength(50, ErrorMessage = "Please enter a valid name")]
         public virtual string firstName { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
+        [StringLength(50, ErrorMessage = "Please enter a valid name")]
         public virtual string lastName { get; set; }
 
     }
