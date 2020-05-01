9dad37ef3f0fe1db5cf5b5b0084acc46de582f4e
Jay Singh <s01161093@acad.tri-c.edu>
2020-05-01 00:08:06 -0400

    6b9e3186

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿using MVCMusicStoreApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: Store
+        [HttpGet]
+        public ActionResult Index(int? id)
+        {
+            return View(db.Albums
+                .Where(a => a.GenreId == id)
+                .ToList());
+       
+        }
+
+        // GET: Store/Browse?genre=value
+        [HttpGet]                   
+        public ActionResult Browse()
+        {
+
+            return View(db.Genres.ToList());
+        }
+
+        // GET: Store/
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+
+        }
+      
+    }
+}
